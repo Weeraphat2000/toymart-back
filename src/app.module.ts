610cff4f10322543cf8e3e43935a768e4c3d6249
Weeraphat2000 <weeraphat.main@gmail.com>
2024-04-06 20:42:12 +0700

    init project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AdminModule } from './admin/admin.module';
+import { ProductsModule } from './products/products.module';
+import { UsersModule } from './users/users.module';
+import { WatchListModule } from './watch-list/watch-list.module';
+import { CartModule } from './cart/cart.module';
+import { PageModule } from './page/page.module';
+import { ChatModule } from './chat/chat.module';
+import { TransactionModule } from './transaction/transaction.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [AdminModule, ProductsModule, UsersModule, WatchListModule, CartModule, PageModule, ChatModule, TransactionModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
